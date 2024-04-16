3d61b69ab587caa4e22f2347f5958488328e4f26
banidja <branislav.jaksic@d-one.design>
2024-04-16 13:06:59 +0200

    feat(auth): implement next auth with spotify provider

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth';
+
+import Spotify from 'next-auth/providers/spotify';
+
+const handler = NextAuth({
+  providers: [
+    Spotify({
+      clientId: process.env.SPOTIFY_CLIENT_ID as string,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+      authorization: {
+        params: { scope: 'streaming  user-read-email user-read-private' },
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
