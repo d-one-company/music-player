0c3ed593c6dfbddc5bdd6d292c841ba2ab25cb58
Branislav Jaksic <branislav.jaksic@d-one.design>
2024-04-17 12:36:59 +0200

    feat(trending): pull songs from gloabl top 50 playlist and render them out (#6)
    Co-authored-by: blink-dev <nixy.nikol@gmail.com>

--- components/Track/TrackItem.tsx
+++ components/Track/TrackItem.tsx
@@ -1,34 +1,26 @@
+import { PlaylistedTrack, Track } from '@spotify/web-api-ts-sdk';
 import Image from 'next/image';
-import React from 'react';
 
 type TrackItemProps = {
-  track: {
-    id: number;
-    title: string;
-    artist: string;
-    img: string;
-    duration: string;
-  };
+  track: PlaylistedTrack<Track>;
 };
 
-const TrackItem = ({
-  track: { id, title, artist, img, duration },
-}: TrackItemProps) => {
+const TrackItem = ({ track }: TrackItemProps) => {
   return (
-    <div className="flex items-center gap-2">
-      <p className="w-20 px-4 text-muted-foreground">{id}</p>
-      <div className="flex items-center gap-4">
-        <Image
-          className="shrink-0 grow-0 rounded-md"
-          src={img}
-          alt={title}
-          width={65}
-          height={65}
-        />
-        <div className="flex w-[300px] flex-col">
-          <p className="max-w-full truncate text-sm">{title}</p>
-          <p className="text-sm text-muted-foreground">{artist}</p>
-        </div>
+    <div className="flex items-center gap-4">
+      <Image
+        className="shrink-0 grow-0 rounded-md"
+        //todo: check URL
+        src={track.track.album.images[0].url}
+        alt={track.track.name}
+        width={65}
+        height={65}
+      />
+      <div className="flex w-[300px] flex-col">
+        <p className="max-w-full truncate text-sm">{track.track.name}</p>
+        <p className="text-sm text-muted-foreground">
+          {track.track.artists.map(artist => artist.name).join(', ')}
+        </p>
       </div>
     </div>
   );
---
+++ components/TrendingSongs/TrendingSongs.tsx
@@ -0,0 +1,24 @@
+import spotifyApi from '@/lib/spotify';
+import { cn } from '@/lib/utils';
+import TrackItem from '../Track/TrackItem';
+
+const GLOBAL_TOP_50_PLAYLIST_ID =
+  '37i9dQZEVXbMDoHDwVN2tF?si=18e6ab1f9a6541a8&nd=1&dlsi=9c44c461677f477f';
+
+const TrendingSongs = async () => {
+  const trendingSongs = await spotifyApi.playlists.getPlaylist(
+    GLOBAL_TOP_50_PLAYLIST_ID
+  );
+
+  return (
+    <div
+      className="scrollbar-sky flex max-h-[300px] flex-col space-y-5 overflow-y-scroll"
+    >
+      {trendingSongs.tracks.items.map(track => (
+        <TrackItem track={track} key={track.track.id} />
+      ))}
+    </div>
+  );
+};
+
+export default TrendingSongs;
