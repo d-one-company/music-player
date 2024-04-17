173e822b8af5d49c7f6a7506d6eab93441230550
Branislav Jaksic <branislav.jaksic@d-one.design>
2024-04-17 17:24:49 +0200

    feat: pull currently playing song and display it (#8)

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -7,7 +7,10 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.SPOTIFY_CLIENT_ID as string,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
       authorization: {
-        params: { scope: 'streaming user-read-email user-read-private' },
+        params: {
+          scope:
+            'streaming playlist-read-private playlist-read-collaborative user-read-email user-read-private user-read-playback-state user-modify-playback-state user-read-currently-playing',
+        },
       },
       profile(profile) {
         return {
@@ -20,13 +23,21 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user, account }) {
+      if (account?.access_token && account?.expires_at) {
+        token.accessToken = account.access_token;
+        token.accessTokenExpires = account.expires_at;
+      }
+
       if (user) token.spotifyUserId = user.id;
+
       return token;
     },
     async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
       if (session.user)
         session.user.spotifyUserId = token.spotifyUserId as string;
+
       return session;
     },
   },
