b8d467206b3271b0d9bcdc9d14ab64c399b76ff6
igorBudimirDev <119533190+igorBudimirDev@users.noreply.github.com>
2024-04-17 21:19:58 +0200

    hoftix: layout (#13)

--- components/TrendingSongs/TrendingSongs.tsx
+++ components/TrendingSongs/TrendingSongs.tsx
@@ -5,7 +5,7 @@ const TrendingTracks = async () => {
   const trendingTracks = await getTrendingTracks();
 
   return (
-    <div className="scrollbar-sky flex max-h-[250px] flex-col space-y-5 overflow-y-scroll">
+    <div className="scrollbar-sky flex max-h-[200px] flex-col space-y-5 overflow-y-scroll 2xl:max-h-[300px]">
       {trendingTracks.tracks.items.map(track => (
         <TrackItem track={track} key={track.track.id} />
       ))}
