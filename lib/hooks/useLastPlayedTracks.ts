017ccd12cfc1dbd13966a078bae253b74ad6ce5b
banidja <branislav.jaksic@d-one.design>
2024-04-18 10:37:27 +0200

    fix: display last played songs if no current song playing

--- lib/hooks/useLastPlayedTracks.ts
+++ lib/hooks/useLastPlayedTracks.ts
@@ -3,27 +3,27 @@ import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import { createSpotifyInstance } from '../helpers/createSpotifyInstance';
 
-export function useQueuedTracks() {
+export function useLastPlayedTracks() {
   const { data: session } = useSession();
-  const [queuedTracks, setQueuedTracks] = useState<Track[]>();
+  const [lastPlayedTracks, setLastPlayedTracks] = useState<Track[]>();
 
   useEffect(() => {
-    async function fetchCurrentlyPlaying() {
+    async function fetchLastPlayedTrack() {
       if (!session) return;
+
       const spotifyApi = createSpotifyInstance(session);
 
       try {
-        const response = await spotifyApi?.player.getUsersQueue();
-        setQueuedTracks(response?.queue as Track[]);
+        const response = await spotifyApi?.player.getRecentlyPlayedTracks();
+
+        setLastPlayedTracks(response?.items.map(item => item.track) as Track[]);
       } catch (error) {
         console.error(error);
       }
     }
 
-    if (session?.accessToken && session?.expires) {
-      fetchCurrentlyPlaying();
-    }
+    if (session?.accessToken && session?.expires) fetchLastPlayedTrack();
   }, [session?.accessToken, session?.expires, session]);
 
-  return queuedTracks;
+  return lastPlayedTracks;
 }
