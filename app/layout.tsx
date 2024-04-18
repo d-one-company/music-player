0515a27cf329c09a4c32aed09155f499fba7f24c
banidja <branislav.jaksic@d-one.design>
2024-04-18 15:44:39 +0200

    merge staging

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { PlayerProvider } from '@/providers/PlayerContext';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import type { PropsWithChildren } from 'react';
@@ -13,7 +14,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <PlayerProvider>{children}</PlayerProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import { Dashboard } from '@/components/Dashboard/Dashboard';
 import PlaylistBar from '@/components/PlaylistBar/PlaylistBar';
-import { PlayerProvider } from '@/providers/PlayerContext';
 
 export default function Home() {
   return (
     <div className="flex h-screen overflow-hidden">
-      <PlayerProvider>
-        <Dashboard />
-        <PlaylistBar />
-      </PlayerProvider>
+      <Dashboard />
+      <PlaylistBar />
     </div>
   );
 }
