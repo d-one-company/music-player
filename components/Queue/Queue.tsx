5f828e718da177a315e2ad945b1ac60f2196f003
igorBudimirDev <119533190+igorBudimirDev@users.noreply.github.com>
2024-04-18 14:28:06 +0200

    style: cards ui bugs (#17)

--- components/Queue/Queue.tsx
+++ components/Queue/Queue.tsx
@@ -7,7 +7,7 @@ const Queue = () => {
   const queuedTracks = generateFakeTracks({ count: 10 });
 
   return (
-    <div className="flex max-h-[calc(100%-320px)] flex-col overflow-y-auto">
+    <div className="flex max-h-[calc(100%-320px)] max-w-full flex-col overflow-y-auto">
       <p className="pb-2 text-left text-lg font-semibold">Queue</p>
       <div className="flex flex-col space-y-6 pt-8">
         {queuedTracks?.map(track => (
--- components/Queue/QueuedTrack.tsx
+++ components/Queue/QueuedTrack.tsx
@@ -7,24 +7,27 @@ type Props = { track: Track };
 
 const QueuedTrack = ({ track }: Props) => {
   return (
-    <div className="flex items-center justify-between" key={track.id + 1}>
+    <div
+      className="flex max-w-full items-center justify-between"
+      key={track.id + 1}
+    >
       <div className="flex gap-3">
         <Image
           src={track.img}
           alt="track"
           width={65}
           height={65}
-          className="grow-0 rounded-sm"
+          className="shrink-0 grow-0 rounded-sm"
         />
-        <div className="flex flex-col justify-center space-y-1">
-          <p className="font-light">{track.title}</p>
+        <div className="flex max-w-full flex-col justify-center space-y-1">
+          <p className="line-clamp-1 text-ellipsis font-light">{track.title}</p>
           <p className="text-sm font-light text-muted-foreground">
             {track.artist}
           </p>
         </div>
       </div>
-      <Button variant="ghost" className="group rounded-full p-2">
-        <EllipsisVertical className="h-6 w-6 text-muted-foreground transition-colors group-hover:text-primary" />
+      <Button variant="ghost" className="group mx-1.5 rounded-full p-1">
+        <EllipsisVertical className="h-5 w-5 text-muted-foreground transition-colors group-hover:text-primary" />
       </Button>
     </div>
   );
