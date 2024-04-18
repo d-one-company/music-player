4b9fc992bd32943cb360eff497b7b044dab017bc
vukasin <vukasin.mladenovic@d-one.design>
2024-04-18 17:08:13 +0200

    feat: display recent favourites

--- components/RecentFavourites/RecentFavouritesList.tsx
+++ components/RecentFavourites/RecentFavouritesList.tsx
@@ -1,13 +1,19 @@
 'use client';
+
+import useTrackStore from '@/lib/store';
 import RecentFavouriteListItem from './RecentFavouriteListItem';
 import { tracks } from '@/lib/tracks';
 
 const RecentFavouritesList = () => {
+  const { favoriteTrackIds } = useTrackStore();
+
   return (
     <div className="flex items-center gap-3 overflow-x-auto">
-      {tracks?.map(track => (
-        <RecentFavouriteListItem key={track.id} track={track} />
-      ))}
+      {tracks
+        .filter(track => favoriteTrackIds.includes(track.id))
+        ?.map(track => (
+          <RecentFavouriteListItem key={track.id} track={track} />
+        ))}
     </div>
   );
 };
