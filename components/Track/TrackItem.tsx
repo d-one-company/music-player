43246632f7a2a96f570187a5316e56a92a5ca36f
Vuukasin <77747830+Vuukasin@users.noreply.github.com>
2024-04-18 17:28:50 +0200

    feat: favorite track toggle logic (#24)

--- components/Track/TrackItem.tsx
+++ components/Track/TrackItem.tsx
@@ -3,17 +3,21 @@
 import { usePlayerContext } from '@/providers/PlayerContext';
 import { Track } from '@/types';
 import Image from 'next/image';
+import useTrackStore from '@/lib/store';
+import { Toggle } from '../ui/toggle';
+import FavoriteIcon from '../icons/FavoriteIcon';
 
 type Props = { track: Track };
 
 const TrackItem = ({ track }: Props) => {
   const { setCurrentTrack } = usePlayerContext();
+  const { favoriteTrackIds, toggleFavorite } = useTrackStore();
 
   return (
     <div
       role="presentation"
       className="flex items-center gap-4"
-      onClick={() => setCurrentTrack(track)}
+      onClick={e => setCurrentTrack(track)}
     >
       <Image
         className="aspect-square rounded-md bg-contain"
@@ -25,6 +29,18 @@ const TrackItem = ({ track }: Props) => {
       <div className="flex w-[300px] flex-col">
         <p className="max-w-full truncate text-sm">{track.title}</p>
         <p className="text-sm text-muted-foreground">{track.artist}</p>
+      </div>
+
+      <div className="ml-auto">
+        <Toggle
+          onClick={e => e.stopPropagation()}
+          variant="favorite"
+          className="h-10 w-10"
+          pressed={favoriteTrackIds.includes(track.id)}
+          onPressedChange={() => toggleFavorite(track.id)}
+        >
+          <FavoriteIcon />
+        </Toggle>
       </div>
     </div>
   );
