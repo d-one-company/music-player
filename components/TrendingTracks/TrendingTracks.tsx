9a944419140d851a9e62c386650373320c86a439
vukasin <vukasin.mladenovic@d-one.design>
2024-04-19 16:51:22 +0200

    feat: track item design & additional info

--- components/TrendingTracks/TrendingTracks.tsx
+++ components/TrendingTracks/TrendingTracks.tsx
@@ -3,9 +3,16 @@ import TrackItem from '../Track/TrackItem';
 
 const TrendingTracks = () => {
   return (
-    <div className="scrollbar-sky flex max-h-[200px] flex-col space-y-5 overflow-y-scroll 2xl:max-h-[300px]">
-      {tracks.map(track => (
-        <TrackItem track={track} key={track.id} />
+    <div className="scrollbar-sky flex max-h-[200px] flex-col overflow-y-scroll 2xl:max-h-[300px]">
+      {tracks.map((track, idx) => (
+        <div className="group flex gap-4" key={track.id}>
+          <div className="hidden items-center justify-center lg:flex">
+            <span className="text-sm text-muted-foreground group-hover:text-white">
+              {idx < 9 ? `0${idx + 1}` : idx + 1}
+            </span>
+          </div>
+          <TrackItem track={track} key={track.id} />
+        </div>
       ))}
     </div>
   );
