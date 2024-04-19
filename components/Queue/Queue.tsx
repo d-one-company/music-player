54d4096500cce11586dd987608db753bedc33398
Branislav Jaksic <branislav.jaksic@d-one.design>
2024-04-19 16:30:48 +0200

    fix: left sidebar design: (#29)

--- components/Queue/Queue.tsx
+++ components/Queue/Queue.tsx
@@ -1,16 +1,22 @@
 'use client';
 
 import { tracks } from '@/lib/tracks';
+import { usePlayerContext } from '@/providers/PlayerContext';
 import QueuedTrack from './QueuedTrack';
 
 const Queue = () => {
+  const { currentPlaylist } = usePlayerContext();
   return (
-    <div className="flex max-h-[calc(100%-320px)] w-full max-w-full flex-col overflow-y-auto px-4">
-      <p className="pb-2 text-left text-lg font-semibold">Queue</p>
-      <div className="flex flex-col space-y-2 pt-8">
+    <>
+      <div className="flex w-full px-4 pb-2">
+        <p className="text-left text-lg font-semibold">
+          {currentPlaylist ? `Next on ${currentPlaylist.title}` : 'Queue'}
+        </p>
+      </div>
+      <div className="flex max-h-[calc(100%-320px)] w-full max-w-full flex-col space-y-2 overflow-y-auto px-4 pt-2">
         {tracks?.map(track => <QueuedTrack track={track} key={track.id} />)}
       </div>
-    </div>
+    </>
   );
 };
 
