8c31321f6a5dbd502f7cd1ee654967812e04f077
banidja <branislav.jaksic@d-one.design>
2024-04-19 16:27:00 +0200

    fix: left sidebar design:

--- components/PlaylistBar/PlaylistBar.tsx
+++ components/PlaylistBar/PlaylistBar.tsx
@@ -1,17 +1,9 @@
-import { X } from 'lucide-react';
 import Player from '../Player/Player';
 import Queue from '../Queue/Queue';
-import { Button } from '../ui/button';
 
 const PlaylistBar = () => {
   return (
-    <div className="relative flex min-h-screen shrink-0 grow flex-col items-center justify-center border bg-muted/50 py-8 pt-24 md:w-[350px]">
-      <Button
-        variant="ghost"
-        className="group absolute right-6 top-6 h-12 w-12 rounded-full p-2"
-      >
-        <X className="h-9 w-9 text-muted-foreground transition-colors group-hover:text-primary" />
-      </Button>
+    <div className="relative flex min-h-screen shrink-0 grow flex-col items-center justify-center border bg-muted/50 py-8 md:w-[350px]">
       <Queue />
       <Player />
     </div>
