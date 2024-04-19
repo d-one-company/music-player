005421fea29c7895e1eff01fe280794e23cd7e9e
banidja <branislav.jaksic@d-one.design>
2024-04-19 18:08:31 +0200

    feat: add more songs and playlists

--- lib/playlists.ts
+++ lib/playlists.ts
@@ -1,8 +1,13 @@
-import type { Playlist } from '@/types';
-import { tracks } from './tracks';
 import Gradient_1 from '@/public/images/gradient1.jpg';
 import Gradient_2 from '@/public/images/gradient2.jpg';
 import Gradient_3 from '@/public/images/gradient3.jpg';
+import Gradient_4 from '@/public/images/gradient4.jpg';
+import Gradient_5 from '@/public/images/gradient5.jpg';
+import Gradient_6 from '@/public/images/gradient6.jpg';
+import Gradient_7 from '@/public/images/gradient7.jpg';
+
+import type { Playlist } from '@/types';
+import { tracks } from './tracks';
 
 export const playlists: Playlist[] = [
   {
@@ -22,5 +27,29 @@ export const playlists: Playlist[] = [
     title: 'Rock',
     image: Gradient_3.src,
     tracks: tracks.slice(5, 7),
+  },
+  {
+    id: 4,
+    title: 'Indie-Pop',
+    image: Gradient_4.src,
+    tracks: tracks.slice(7, 13),
+  },
+  {
+    id: 5,
+    title: 'Jazz',
+    image: Gradient_5.src,
+    tracks: tracks.slice(13, 20),
+  },
+  {
+    id: 6,
+    title: 'Party',
+    image: Gradient_6.src,
+    tracks: tracks.slice(20, 24),
+  },
+  {
+    id: 7,
+    title: 'Metal',
+    image: Gradient_7.src,
+    tracks: tracks.slice(24, 29),
   },
 ];
--- lib/tracks.ts
+++ lib/tracks.ts
@@ -2,6 +2,18 @@ import Image_1 from '@/public/images/image1.jpg';
 import Image_2 from '@/public/images/image2.jpg';
 import Image_3 from '@/public/images/image3.jpg';
 import Image_4 from '@/public/images/image4.jpg';
+import IndiePop_1 from '@/public/images/indie-pop1.jpg';
+import IndiePop_2 from '@/public/images/indie-pop2.jpg';
+import IndiePop_3 from '@/public/images/indie-pop3.jpg';
+import Jazz1 from '@/public/images/jazz1.jpg';
+import Jazz2 from '@/public/images/jazz2.jpg';
+import Jazz3 from '@/public/images/jazz3.jpg';
+import Metal1 from '@/public/images/metal1.jpg';
+import Metal2 from '@/public/images/metal2.jpg';
+import Metal3 from '@/public/images/metal3.jpg';
+import Party1 from '@/public/images/party1.jpg';
+import Party2 from '@/public/images/party2.jpg';
+import Party3 from '@/public/images/party3.jpg';
 
 import type { Track } from '@/types';
 
@@ -75,6 +87,195 @@ export const tracks: Track[] = [
     artist: 'Laura Brehm',
     title: 'Breathe',
     duration: '3:45',
-    image: Image_4.src,
+    image: IndiePop_1.src,
+  },
+
+  {
+    id: 9,
+    url: 'https://utfs.io/f/0815cf75-bb80-4f41-ad7c-75988ca7981d-xv5i21.mp3',
+    artist: 'Spektrem',
+    title: 'Shine',
+    duration: '4:45',
+    image: IndiePop_2.src,
+  },
+
+  {
+    id: 10,
+    url: 'https://utfs.io/f/8158cf79-8f64-4bc9-8d8f-5570bf7fba65-f8dtr8.mp3',
+    artist: 'Prismo',
+    title: 'Stronger',
+    duration: '2:22',
+    image: IndiePop_3.src,
+  },
+
+  {
+    id: 11,
+    url: 'https://utfs.io/f/c0d77117-610b-4773-b915-b6730c5a049b-j8f7zj.19.mp3',
+    artist: 'NEFEX',
+    title: 'Careless',
+    duration: '4:01',
+    image: IndiePop_1.src,
+  },
+
+  {
+    id: 12,
+    url: 'https://utfs.io/f/269901ca-1733-419d-8ce2-12e5a98be38c-irb02f.mp3',
+    artist: 'Cartoon, Jeja',
+    title: 'Why We Lose',
+    duration: '3:14',
+    image: Jazz1.src,
+  },
+
+  {
+    id: 13,
+    url: 'https://utfs.io/f/9162d815-9f20-4f5e-9f15-1fa8d40c81e0-f42epx.mp3',
+    artist: 'Twenty One Pilots',
+    title: 'Stressed Out',
+    duration: '4:45',
+    image: Jazz2.src,
+  },
+
+  {
+    id: 15,
+    url: 'https://utfs.io/f/d6cb401e-295f-4af9-946d-e09c3e29f304-ellzeg.mp3',
+    artist: 'Ship Wrek, Zookeepers',
+    title: 'Ark Future',
+    duration: '2:54',
+    image: Jazz3.src,
+  },
+
+  {
+    id: 16,
+    url: 'https://utfs.io/f/8f0cb42e-0bc4-4f9e-bd3b-81bddbafb17e-xadmnu.mp3',
+    artist: 'Unknown Brain',
+    title: 'Superhero',
+    duration: '3:13',
+    image: Jazz1.src,
+  },
+
+  {
+    id: 17,
+    url: 'https://utfs.io/f/f62f7dbb-0acd-4e2b-8f81-dba0e6691544-mckvrt.mp3',
+    artist: 'Cartoon, Jeja',
+    title: 'C U Again',
+    duration: '2:15',
+    image: Jazz2.src,
+  },
+
+  {
+    id: 18,
+    url: 'https://utfs.io/f/3a326059-3c7d-497a-980c-cdb912a8b77f-u8kei.mp3',
+    artist: 'Diviners',
+    title: 'Savannah',
+    duration: '3:45',
+    image: Jazz3.src,
+  },
+
+  {
+    id: 19,
+    url: 'https://utfs.io/f/576f13c2-f1ca-4f0d-815a-2dfbe821bf5a-tpzwgz.mp3',
+    artist: 'Syn Cole',
+    title: 'Feel Good',
+    duration: '2:11',
+    image: Party1.src,
+  },
+
+  {
+    id: 20,
+    url: 'https://utfs.io/f/6df10488-1603-4392-b9bc-2c9c01dee01c-amoxp8.mp3',
+    artist: 'Unknown Brain',
+    title: 'MATAFAKA',
+    duration: '2:41',
+    image: Party2.src,
+  },
+
+  {
+    id: 21,
+    url: 'https://utfs.io/f/9f94560a-a56b-4f28-ad68-b84506147cdb-stlg3h.mp3',
+    artist: 'Dr Dre',
+    title: 'The Next Episode',
+    duration: '4:10',
+    image: Party3.src,
+  },
+
+  {
+    id: 22,
+    url: 'https://utfs.io/f/576f13c2-f1ca-4f0d-815a-2dfbe821bf5a-tpzwgz.mp3',
+    artist: 'Syn Cole',
+    title: 'Feel Good Future',
+    duration: '4:02',
+    image: Party1.src,
+  },
+
+  {
+    id: 23,
+    url: 'https://utfs.io/f/e2a0d9d4-9a46-4aac-8c6c-2a8c9d216c63-rdx7ei.mp3',
+    artist: 'Jo Cohen, Sex Whales',
+    title: 'We Are',
+    duration: '3:11',
+    image: Party2.src,
+  },
+
+  {
+    id: 24,
+    url: 'https://utfs.io/f/89a4f92e-759a-4348-999b-67426be27f35-6uyol8.mp3',
+    artist: 'Janji',
+    title: 'Heroes Tonight',
+    duration: '3:44',
+    image: Party3.src,
+  },
+
+  {
+    id: 25,
+    url: 'https://utfs.io/f/c2daa869-2198-4059-8ce6-77f57322d0b6-gt9twy.mp3',
+    artist: 'Lost Sky',
+    title: 'Dreams',
+    duration: '3:10',
+    image: Metal1.src,
+  },
+
+  {
+    id: 26,
+    url: 'https://utfs.io/f/8c3ee31a-66d0-4382-bc65-4226db00b6be-ra2daj.mp3',
+    artist: 'Max Brhon',
+    title: 'Bass Midtempo',
+    duration: '4:07',
+    image: Metal2.src,
+  },
+
+  {
+    id: 27,
+    url: 'https://utfs.io/f/8a31e6c4-5a38-4118-aa1c-d937ae03c6b2-pnb462.mp3',
+    artist: 'OSKI',
+    title: "What's the Problem",
+    duration: '2:11',
+    image: Metal3.src,
+  },
+
+  {
+    id: 28,
+    url: 'https://utfs.io/f/dd86ddb5-4148-4521-a79b-71e0341f6d6a-3sxuwv.mp3',
+    artist: 'Shiah',
+    title: 'Done Better',
+    duration: '3:13',
+    image: Metal1.src,
+  },
+
+  {
+    id: 29,
+    url: 'https://utfs.io/f/dfdab19e-6824-45f9-b5ab-470dc1aa35e7-8p6w24.mp3',
+    artist: 'Tatsunoshin',
+    title: 'Lost My Love',
+    duration: '4:14',
+    image: Metal2.src,
+  },
+
+  {
+    id: 30,
+    url: 'https://utfs.io/f/ac39d249-68c5-4187-938d-3b006083559c-v0hj2v.mp3',
+    artist: 'Warrio',
+    title: 'Mortals',
+    duration: '5:00',
+    image: Metal3.src,
   },
 ];
