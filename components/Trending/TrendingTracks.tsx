96b51e3b8e51ef93ce1d2a04689eff2c7e27c8ed
banidja <branislav.jaksic@d-one.design>
2024-04-20 19:55:01 +0200

    feat: move images to uploadthing

--- components/Trending/TrendingTracks.tsx
+++ components/Trending/TrendingTracks.tsx
@@ -1,10 +1,14 @@
 import { tracks } from '@/lib/tracks';
+import { usePlayerContext } from '@/providers/PlayerContext';
 import TrackItem from '../Track/TrackItem';
 
 const TrendingTracks = () => {
+  const { currentPlaylist } = usePlayerContext();
+  const currentTracks = currentPlaylist ? currentPlaylist?.tracks : tracks;
+
   return (
     <div className="scrollbar-sky flex max-h-[200px] flex-col overflow-y-scroll 2xl:max-h-[300px]">
-      {tracks.map((track, idx) => (
+      {currentTracks?.map((track, idx) => (
         <div className="group flex gap-4" key={track.id}>
           <div className="hidden items-center justify-center lg:flex">
             <span className="text-sm text-muted-foreground group-hover:text-white">
