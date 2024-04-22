63f42ee6248d0787a3642bde9ec1c80c01b3ecf8
Vuukasin <77747830+Vuukasin@users.noreply.github.com>
2024-04-22 18:12:24 +0200

    fix: layout on favourites page (#46)

--- app/favorite/page.tsx
+++ app/favorite/page.tsx
@@ -5,8 +5,7 @@ import PlaylistBar from '@/components/PlaylistBar/PlaylistBar';
 
 const Page = () => {
   return (
-    <div className="flex h-screen flex-wrap overflow-hidden">
-      <Sidebar />
+    <>
       <div className="grid h-screen flex-1 grid-rows-content gap-5 bg-background px-4 pt-4 sm:gap-8 sm:px-8 sm:pt-8">
         <Header />
         <main className="flex grow flex-col gap-2 overflow-hidden">
@@ -15,7 +14,7 @@ const Page = () => {
         </main>
       </div>
       <PlaylistBar />
-    </div>
+    </>
   );
 };
 
