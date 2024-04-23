837150f90567582affd9d103759924f7c3cef64b
Vuukasin <77747830+Vuukasin@users.noreply.github.com>
2024-04-23 13:16:28 +0200

    feat: implement search on all pages (#53)

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -3,16 +3,31 @@
 import { Input } from '@/components/ui/input';
 import { usePlayerContext } from '@/providers/PlayerContext';
 import { Search as SearchIcon } from 'lucide-react';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useRef } from 'react';
 
 const Search = () => {
   const { search, setSearch } = usePlayerContext();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const pathname = usePathname();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!inputRef.current) return;
+    setSearch('');
+    if (pathname === '/search') {
+      inputRef.current.focus();
+    }
+  }, [pathname, setSearch]);
 
   return (
     <div className="relative flex flex-1 items-center gap-2">
       <SearchIcon className="absolute left-3.5 top-3.5 h-4 w-4 text-muted-foreground" />
       <Input
         value={search}
+        ref={inputRef}
         onChange={e => setSearch(e.target.value)}
+        onFocus={() => pathname === '/' && router.push('/search')}
         type="search"
         placeholder="Search for tracks, artists, lyrics..."
         className="h-11 w-full rounded-full bg-muted pl-10"
--- components/Trending/TrendingTracks.tsx
+++ components/Trending/TrendingTracks.tsx
@@ -10,23 +10,29 @@ type Props = {
 };
 
 const TrendingTracks = ({ className }: Props) => {
-  const { currentPlaylist } = usePlayerContext();
+  const { currentPlaylist, search } = usePlayerContext();
   const currentTracks = currentPlaylist ? currentPlaylist?.tracks : tracks;
 
   return (
     <div
       className={cn('scrollbar-sky flex flex-col overflow-y-scroll', className)}
     >
-      {currentTracks?.map((track, idx) => (
-        <div className="group flex gap-4" key={track.id}>
-          <div className="hidden items-center justify-center lg:flex">
-            <span className="text-sm text-muted-foreground group-hover:text-white">
-              {idx < 9 ? `0${idx + 1}` : idx + 1}
-            </span>
+      {currentTracks
+        ?.filter(
+          track =>
+            track.title.toLowerCase().includes(search) ||
+            track.artist.toLowerCase().includes(search)
+        )
+        .map((track, idx) => (
+          <div className="group flex gap-4" key={track.id}>
+            <div className="hidden items-center justify-center lg:flex">
+              <span className="text-sm text-muted-foreground group-hover:text-white">
+                {idx < 9 ? `0${idx + 1}` : idx + 1}
+              </span>
+            </div>
+            <TrackItem track={track} key={track.id} />
           </div>
-          <TrackItem track={track} key={track.id} />
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
