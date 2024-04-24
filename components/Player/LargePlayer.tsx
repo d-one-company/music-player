925ea5e8786c6abbbe25e9cf97728b646180f748
blink-dev <nikola.mirjanic@d-one.design>
2024-04-24 11:25:40 +0200

    feat: continious playback during navigation

--- components/Player/LargePlayer.tsx
+++ components/Player/LargePlayer.tsx
@@ -3,7 +3,6 @@
 import { usePlayerContext } from '@/providers/PlayerContext';
 import { ArrowLeftToLine, ArrowRightToLine, Pause, Play } from 'lucide-react';
 import Image from 'next/image';
-import { useEffect, useRef, useState } from 'react';
 import { Button } from '../ui/button';
 import { Slider } from '../ui/slider';
 
@@ -14,38 +13,15 @@ const LargePlayer = () => {
     handlePlayNext,
     handlePlayPrev,
     currentTrack,
+    currentTime,
+    duration,
+    changeTime,
   } = usePlayerContext();
-  const audioRef = useRef<HTMLAudioElement>(null);
-  const [currentTime, setCurrentTime] = useState<number | undefined>(0);
-
-  useEffect(() => {
-    if (audioRef.current)
-      isPlaying ? audioRef.current.play() : audioRef.current.pause();
-  }, [isPlaying, currentTrack]);
-
-  useEffect(() => {
-    if (!audioRef.current) return;
-
-    const handler = () => setCurrentTime(audioRef.current?.currentTime);
-
-    audioRef.current.addEventListener('timeupdate', handler);
-
-    return () => {
-      if (audioRef.current)
-        audioRef.current.removeEventListener('timeupdate', handler);
-    };
-  }, [audioRef.current]);
 
   if (!currentTrack) return null;
 
   return (
     <div className="fixed bottom-0 left-0 right-0 flex h-20 items-center bg-black px-4 text-white">
-      <audio
-        autoPlay
-        ref={audioRef}
-        src={currentTrack.url}
-        onEnded={handlePlayNext}
-      />
       <div className="mr-4 flex h-full w-3/12 min-w-[160px] items-center">
         <Image
           width={50}
@@ -63,10 +39,8 @@ const LargePlayer = () => {
       <div className="flex-grow">
         <Slider
           defaultValue={[currentTime || 0]}
-          max={audioRef.current?.duration}
-          onValueChange={value => {
-            if (audioRef.current) audioRef.current.currentTime = value[0];
-          }}
+          max={duration}
+          onValueChange={value => changeTime(value[0])}
           value={[currentTime || 0]}
           step={1}
         />
